fee018c7d4c35f794b33e6ae7b90decc8a6f2843
Lamuel27 <samrelliner@gmail.com>
2019-01-02 09:49:39 -0600

    mysql and server work

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Krusty Burger", 0),
+		("Hatfield Hi-Boy", 0),
+        ("Hub City Burger", 0),
+        ("Classic Smash", 0),
+        ("Royale with Cheese", 0);
+        
+SELECT * FROM burgers;
