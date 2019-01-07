2b6f174ac9ec967352f51e6f2e2796ad75fac279
Lamuel27 <samrelliner@gmail.com>
2019-01-06 20:47:42 -0600

    everything works except for adding a new burger

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+
+var connection;
+
+var PORT = process.env.PORT || 8080;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'YourRootPassword',
+		database: 'burgers_db'
+	})
+};
+
+connection.connect(function(err) {
+  console.log("App listening on PORT " + PORT);
+});
+
+module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers (
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
 burger_name VARCHAR (255) NOT NULL,
-devoured BOOLEAN NOT NULL
+devoured BOOLEAN DEFAULT false
 );
 
 
